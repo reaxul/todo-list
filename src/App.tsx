9fc41f1bd3f8b37091a07089bdcf74e56dec0716
DevReaxul <hreazul11@gmail.com>
2024-01-21 20:31:00 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-3xl mt-5">This is todo list</h1>
+      <Button>click</Button>
+    </>
+  )
+}
+
+export default App
