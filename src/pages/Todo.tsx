3b503e4de01d678f904e4f338c33cb1a56a86688
DevReaxul <hreazul11@gmail.com>
2024-01-25 01:12:31 +0600

    Add Container component to Todo page

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,11 +1,12 @@
+import Container from "@/components/ui/Container";
 import TodoContainer from "@/components/ui/todo/TodoContainer";
 
 const Todo = () => {
   return (
-    <div>
+    <Container>
       <h1 className="font-semibold text-2xl text-center">This is todo list</h1>
       <TodoContainer />
-    </div>
+    </Container>
   );
 };
 
