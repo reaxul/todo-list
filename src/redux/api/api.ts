fbb110b3b7c6a1b7ccdda6325afabdfa924f5109
DevReaxul <hreazul11@gmail.com>
2024-02-03 00:37:34 +0600

    Update API base URL and add getTodo endpoint

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,9 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:5173' }),
-    endpoints: () => ({
-
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.example.com' }),
+    endpoints: (builder) => ({
+        getTodo: builder.query({
+            query: () => ({
+                url: '/task',
+                method: 'GET',
+            })
+        }),
     }),
 })
+
+export const { useGetTodoQuery } = baseApi;
